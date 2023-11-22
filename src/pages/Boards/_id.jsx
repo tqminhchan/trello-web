f2690552d94041f6001763f9006433cde88966af
Quang Minh <quangminhbnh1409@gmail.com>
2023-11-23 02:50:31 +0700

    Refactor project structure

---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,19 @@
+// Board details
+import Container from '@mui/material/Container'
+import AppBar from '../../components/AppBar'
+import BoardBar from './BoardBar'
+import BoardContent from './BoardContent'
+
+function Board() {
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <AppBar />
+
+      <BoardBar />
+
+      <BoardContent />
+    </Container>
+  )
+}
+
+export default Board
---
+++ src/redux/store.js
@@ -0,0 +1,1 @@
+// Redux: Stage management tool
