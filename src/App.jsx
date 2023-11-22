aebc5cf53c51d4e59601ef98888562e308c167c4
Quang Minh <quangminhbnh1409@gmail.com>
2023-11-22 11:55:50 +0700

    dark light mode update

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,28 @@ import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
 
+import { useColorScheme } from '@mui/material/styles'
+
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 function App() {
 
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>QuangMinh</div>
 
       <Typography variant="body2" color="text.secondary">test Typography</Typography>
